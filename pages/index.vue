d63a69188891177f8105cd0dd963e96cc0a2fd23
chikawaiii <email.is.not.vaild9@gmail.com>
2025-07-11 19:04:11 +0700

    real

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <UInput v-model="value" placeholder="Site" id="sitename-input" />
+    <UInput v-model="value" placeholder="Page" id="pagename-input" />
+  </div>
+</template>
